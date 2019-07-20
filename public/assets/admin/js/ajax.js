26a85885d1bdc49c791a97a16680f11d27179ac3
PhungMinhHoang <phungminhhoang174@gmail.com>
2019-07-20 18:59:27 +0700

    fix ajax

--- public/assets/admin/js/ajax.js
+++ public/assets/admin/js/ajax.js
@@ -4,43 +4,35 @@ $.ajaxSetup({
     }
 });
 
+
 $(document).ready(function(){
-    var id;
     $('.edit').click(function(){
         $('.error').hide();
-        id = $(this).data('id');
-        $.ajax({
-            url: `admin/category/${id}/edit`,
-            dataType: 'json',
-            type: 'get',
-            success: function(result){
-                console.log(result)
-                $('.name').val(result.name);
-                $('.title').text(result.name);
-                if(result.status == 1)
-                {   
-                    $('.kht').removeAttr('selected')
-                    $('.ht').attr('selected','selected') 
-                }
-                else
-                {   
-                    $('.ht').removeAttr('selected')
-                    $('.kht').attr('selected','selected')
-                    
-                }
+        let id = $(this).data('id');
+        $.get(`admin/category/${id}/edit`)
+        .done(function(result){
+            $('.name').val(result.name);
+            $('.title').text(result.name);
+            if(result.status == 1)
+            {   
+                $('.kht').removeAttr('selected')
+                $('.ht').attr('selected','selected') 
             }
+            else
+            {   
+                $('.ht').removeAttr('selected')
+                $('.kht').attr('selected','selected')
+                
+            }
+            SaveClick(id)
         })
-    })
-    SaveClick(id)
+    }) 
 })
 
 function SaveClick(id){
-    console.log(id)
     $('.update').click(function(){
-        let i = 1;
         let name = $('.name').val()
         let status = $('.status').val()
-        
         $.ajax({
             url: `admin/category/${id}`,
             data: {
@@ -49,18 +41,18 @@ function SaveClick(id){
             },
             type:'put',
             dataType: 'json',
-            success: function(result){
-                if(result.error)
-                {
-                    $('.error').show();
-                    $('.error').html(result.message.name[0]);
-                }
-                else
-                {
-                    toastr.success(result.success,'Thông báo', {timeOut: 5000})
-                    $('#edit').modal('hide')
-                    location.reload()
-                }
+        })
+        .done(function(result){
+            if(result.error)
+            {
+                $('.error').show();
+                $('.error').html(result.message.name[0]);
+            }
+            else
+            {
+                toastr.success(result.success,'Thông báo', {timeOut: 5000})
+                $('#edit').modal('hide')
+                location.reload()
             }
         })
     })
